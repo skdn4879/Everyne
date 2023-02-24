fea4ac5b52cef8ee990a5c4941f4b94094777946
skdn4879 <skanxkrl3645@naver.com>
2023-02-25 01:42:08 +0900

    init

---
+++ src/main/java/com/exam/everyne/EveryneApplication.java
@@ -0,0 +1,13 @@
+package com.exam.everyne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/everyne/EveryneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.everyne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
